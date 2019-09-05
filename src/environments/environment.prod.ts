f9a2bd5354003be8774c3c92ae983ae4c5fe4a2f
gururam@mailinator.com <gururam@mailinator.com>
2019-09-06 01:19:08 +0530

    make it better

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase:{
-    apiKey: "AIzaSyBLNc6qaUT9uk81jHwQy4Cpk2PDHAPedpc",
-    authDomain: "shop-shure.firebaseapp.com",
-    databaseURL: "https://shop-shure.firebaseio.com",
-    projectId: "shop-shure",
-    storageBucket: "shop-shure.appspot.com",
-    messagingSenderId: "832191408292"
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase:{
-    apiKey: "AIzaSyBLNc6qaUT9uk81jHwQy4Cpk2PDHAPedpc",
-    authDomain: "shop-shure.firebaseapp.com",
-    databaseURL: "https://shop-shure.firebaseio.com",
-    projectId: "shop-shure",
-    storageBucket: "shop-shure.appspot.com",
-    messagingSenderId: "832191408292"
-  }
+  production: false
 };
 
 /*
