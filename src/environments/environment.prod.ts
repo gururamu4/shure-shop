83782ba1ef6857f8c72ccec9e7ee0c86f9c810e3
gururam@mailinator.com <gururam@mailinator.com>
2019-09-06 01:14:30 +0530

    make it better

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBLNc6qaUT9uk81jHwQy4Cpk2PDHAPedpc",
+    authDomain: "shop-shure.firebaseapp.com",
+    databaseURL: "https://shop-shure.firebaseio.com",
+    projectId: "shop-shure",
+    storageBucket: "shop-shure.appspot.com",
+    messagingSenderId: "832191408292"
+  }
+};
